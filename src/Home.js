c29c3ed6e539291165b398bc3ed878436723daf8
Prasheekmankar <89675381+Prasheekmankar@users.noreply.github.com>
2022-03-20 11:37:05 +0530

    commit change

--- src/Home.js
+++ src/Home.js
@@ -6,10 +6,10 @@ export function Home() {
       <h3>Hi,</h3>
       <img src="https://media2.giphy.com/media/ujrj9aoOdNvXO/giphy.gif?cid=ecf05e478a2jrz2j1o9lf1jy7f5got2l65axwbxwzd1yu3j8&rid=giphy.gif&ct=s.gif" alt="hellow" width="200" height="100"></img>
 
-      <h1>My name is Vrushabh Mungelwar</h1>
+      <h1>My name is Prasheek BUddheshwar Mankar</h1>
       <h2>I am a Full-Stack Developer</h2>
       <Button
-        href="https://drive.google.com/file/d/1jmuhJlK80jx8-zlrsNOA0qQewRGTFvap/view"
+        href=""
         color="warning"
         variant="contained"
         target="_blank"
